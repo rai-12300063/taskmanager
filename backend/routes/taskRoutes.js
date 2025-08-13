6830b0dab8338c984c5b59aeb56db37bc8fd1432
rai-12300063 <racilarde7678@gmail.com>
2025-08-14 05:20:19 +1000

    push git changes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,4 +1,4 @@
-
+/*
 const express = require('express');
 const { getTasks, addTask, updateTask, deleteTask } = require('../controllers/taskController');
 const { protect } = require('../middleware/authMiddleware');
@@ -7,4 +7,15 @@ const router = express.Router();
 router.route('/').get(protect, getTasks).post(protect, addTask);
 router.route('/:id').put(protect, updateTask).delete(protect, deleteTask);
 
+module.exports = router;
+*/
+
+const express = require("express")
+const {getTasks,addTask, updateTask, deleteTask} = require('../controllers/taskController');
+const { protect } = require('../middleware/authMiddleware');
+const router = express.Router()
+router.get("/", protect, getTasks)
+router.post("/", protect, addTask)
+router.put("/", protect, updateTask);
+router.delete("/", protect, deleteTask);
 module.exports = router;
